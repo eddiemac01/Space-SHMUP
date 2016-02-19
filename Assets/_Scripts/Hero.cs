b503b6c7f6e8539591182691ef26fbab2567f24a
unknown <EDDIEMCD@CILABS-119B-02.scilsnet.rutgers.edu>
2016-02-18 20:51:12 -0500

    Second SHMUP commit, added sheild

--- Assets/_Scripts/Hero.cs
+++ Assets/_Scripts/Hero.cs
@@ -2,14 +2,34 @@
 using System.Collections;
 
 public class Hero : MonoBehaviour {
+	static public Hero		S; //Singleton
 
-	// Use this for initialization
-	void Start () {
-	
+	//These fields control the movement of the ship
+	public float			speed = 30;
+	public float			rollMult = -45;
+	public float			pitchMult = 30;
+
+	//Ship statud information
+	public float			shieldLevel = 1;
+
+	public bool				______________________;
+
+	void Awake() {
+		S = this; //Set the Singleton
 	}
-	
-	// Update is called once per frame
+
 	void Update () {
-	
+		//Pull in information tfrom the Input class
+		float xAxis = Input.GetAxis ("Horizontal");
+		float yAxis = Input.GetAxis ("Vertical");
+
+		//Change transform.position based on the axes
+		Vector3 pos = transform.position;
+		pos.x += xAxis * speed * Time.deltaTime;
+		pos.y += yAxis * speed * Time.deltaTime;
+		transform.position = pos;
+
+		//Rotate the ship to make it feel more dynamic
+		transform.rotation = Quaternion.Euler (yAxis * pitchMult, xAxis * rollMult, 0);
 	}
 }
